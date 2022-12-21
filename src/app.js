5d8485644325067452a880433a7f5bc42af8675c
Sebastnt <sebastnt.p@hotmail.com>
2022-12-21 19:44:28 +0000

    Excuse generator project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#the-excuse").innerHTML = excuse();
   console.log("Hello Rigo from the console!");
+};
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let excuse = () => {
+  let randomNumber1 = Math.floor(Math.random() * who.length);
+
+  let randomNumber2 = Math.floor(Math.random() * action.length);
+
+  let randomNumber3 = Math.floor(Math.random() * what.length);
+
+  let randomNumber4 = Math.floor(Math.random() * when.length);
+  return (
+    who[randomNumber1] +
+    " " +
+    action[randomNumber2] +
+    " " +
+    what[randomNumber3] +
+    " " +
+    when[randomNumber4]
+  );
 };
